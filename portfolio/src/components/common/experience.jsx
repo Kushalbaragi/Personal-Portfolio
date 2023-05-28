10a31d6f0dfb499cf4c2723ac5b936262271df85
Kushal baragi <Kushalbaragi83@gmail.com>
2023-05-29 00:01:21 +0530

    project page implmentation

---
+++ portfolio/src/components/common/experience.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ExperienceCard from './experienceCard'
+
+function Experience() {
+  return (
+ <>
+ <p className='disclaimer'>*These are not my owned projects,  so i cannot share the source code.  However you can check the site that are live on the internet and We worked as a team being in <strong>Mindtree</strong> to develop these e-commerce projects.</p>
+ <ExperienceCard/>
+ <ExperienceCard/>
+ <ExperienceCard/>
+ </>
+  )
+}
+
+export default Experience
